9f203dc17bc9979df1c39cd9dd0bd476eb42b6f5
Katherina611 <katarzynag0609@gmail.com>
2018-03-10 12:31:14 +0100

    arrays method, js object

--- javascript-basic/app.js
+++ javascript-basic/app.js
@@ -167,5 +167,82 @@ console.log(Array.isArray(months));
 //change values of the array
 months[1] = 'New Months';
 console.log(months);
-//find sth in array
+//find sth in the array
 console.log(months.indexOf('January'));
+//add more items in the array (end of the array)
+months.push("May");
+//add more items into the array (at the beginning)
+months.unshift("Month 0");
+//add an element in the middle
+months.splice(3,null,"a new month");
+//remove element from the end
+months.pop();
+//remove element from the beginning
+months.shift();
+//remove element from the middle of the array
+months.splice(2,1);
+//array methods
+//reverse
+months.reverse();
+console.log(months);
+//concatenate two arrays
+const array1=[1,2,3];
+      array2=[7,8,9];
+console.log(array1.concat(array2));
+//order an array (sort)
+let fruits = ["banana", "apple", "strawberry", "orange", "watermelon"];
+console.log(fruits.sort());
+console.log(fruits.sort().reverse());
+
+const arrayNumber = [3,1,100,5,36,9,60];
+arrayNumber.sort();//it doesnt't work [1, 100, 3, 3, 5, 60, 9]
+//order from lower to the greater number
+arrayNumber.sort(function(a,b){
+    return a-b
+});
+//order from greater to the lower number
+arrayNumber.sort(function(a,b){
+    return b-a
+});
+console.log(arrayNumber);
+
+//javascript Object
+const person = {
+    name: "Kasia",
+    surname: "Gacek",
+    job: "absolvent",
+    email:"kasia@gmail.com",
+    age: 25,
+    living:{
+        city:"Rabka",
+        country:"Poland"
+    },
+    getAge:function(){
+        return 24;
+    },
+    bornYear:function(){
+        return new Date().getFullYear() - this.age;
+    },
+    getJob:function(){
+        return this.job
+    }
+};
+console.log(person.name);//common method
+console.log(person['email']);//second method
+console.log(person.living);
+console.log(person.getAge());
+console.log(person.bornYear());
+console.log(person.getJob());
+
+//array of objects
+let cars = [
+    {model: "Mustang", engine: "6.0"},
+    {model: "Camaro", engine: "6.1"}
+];
+for (let i=0; i<cars.length; i++){
+    console.log(cars[i].model)
+}
+
+
+
+
