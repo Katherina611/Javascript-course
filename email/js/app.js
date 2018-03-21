7e81f0ff334a9258c7f37ab1bacf7e12f8be49db
Katherina611 <katarzynag0609@gmail.com>
2018-03-21 16:39:21 +0100

    added gif

--- email/js/app.js
+++ email/js/app.js
@@ -2,7 +2,9 @@
 const sendBtn = document.querySelector('#sendBtn'),
       email = document.querySelector('#email'),
       subject = document.querySelector('#subject'),
-      message = document.querySelector('#message');
+      message = document.querySelector('#message'),
+      reset = document.querySelector('#resetBtn'),
+      sendEmailForm = document.querySelector('#email-form');
 
 //event listener
 eventListener();
@@ -13,6 +15,9 @@ function eventListener(){
     email.addEventListener('blur', validateField);
     subject.addEventListener('blur', validateField);
     message.addEventListener('blur', validateField);
+    //send email and reset button
+    sendEmailForm.addEventListener('submit', sendEmail);
+    reset.addEventListener('click', resetForm);
 }
 
 //functions
@@ -29,6 +34,17 @@ function validateField(){
     //validate the email
     if(this.type === 'email'){
         validateEmail(this)
+    }
+    //both will return erros, then chech if there're any errors
+    errors = document.querySelectorAll('.error');
+    console.log(errors);
+    //check that the inputs are not empty
+    if(email.value !== '' && subject.value !== '' && message.value !== ''){
+        if(errors.length === 0){
+            //the button should be enabled
+            sendBtn.disabled = false;
+        }
+
     }
 }
 //valifdate the length of the fields
@@ -52,4 +68,34 @@ function validateEmail(field){
         field.style.borderBottomColor = 'red';
         field.classList.add('error');
     }
+}
+//reset the form
+function resetForm(){
+    sendEmailForm.reset();
+
+}
+//send email
+function sendEmail(event){
+    event.preventDefault();
+
+    //show the spinner
+    const spinner = document.querySelector('#spinner');
+    spinner.style.display = 'block';
+    //show the image
+    const sendEmailImg = document.createElement('img');
+    sendEmailImg.src = 'img/mail.gif';
+    sendEmailImg.style.display = 'block';
+    //hide spinner then show sen email image
+    setTimeout(function(){
+        //hide the spinner
+        spinner.style.display = 'none';
+        //show the image
+        document.querySelector('#loaders').appendChild(sendEmailImg);
+        //after 5 s, hide the image and reset the form
+        setTimeout(function(){
+            sendEmailForm.reset();
+            sendEmailImg.remove();
+        },5000);
+    },3000);
+
 }
