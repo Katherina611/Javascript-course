a20810e9d55578c996c00348777e134a15998410
Katherina611 <katarzynag0609@gmail.com>
2018-03-23 11:08:01 +0100

    prototype, create object

--- MoreAboutObject/app.js
+++ MoreAboutObject/app.js
@@ -22,22 +22,88 @@ console.log(client.membership());
 function Client(name, balance){
     this.name = name;
     this.balance = balance;
-    this.membership = function(){
-        let name;
-        if(this.balance > 1000){
-            name = 'Gold';
-        }else if(this.balance > 500){
-            name = 'Platinum';
-        }else{
-            name = 'Normal';
-        }
-        return name;
-    }
+
 }
-const person = new Client('Kasia', 700);
-const personNew = new Client('Karen', 200);
+
+//prototype
+//attach prototype to the method
+Client.prototype.membership = function(){
+    let name;
+    if(this.balance > 1000){
+        name = 'Gold';
+    }else if(this.balance > 500){
+        name = 'Platinum';
+    }else{
+        name = 'Normal';
+    }
+    return name;
+};
+
+//second prototype with the name and balance
+Client.prototype.clientInfo = function(){
+    return `Name: ${this.name}, balance: ${this.balance}, membership: ${this.membership()}`
+};
+
+//another method to withdraw money from the account
+Client.prototype.withdraw = function(amount){
+    this.balance -=amount;
+};
+
+//create a deposite method
+Client.prototype.deposite = function(amount){
+    this.balance += amount;
+};
+
+//check the balance
+Client.prototype.getBalance = function(){
+    return this.balance;
+};
+
+const person = new Client('Kasia', 2000);
+const personNew = new Client('Karen', 600);
+
+console.log(person);
 console.log(person.membership());
+person.withdraw(1000);
+console.log(person.clientInfo());
+console.log(person.getBalance());
+person.deposite(500);
+console.log(person.clientInfo());
+console.log(person.getBalance());
+console.log('-----------');
+console.log(personNew);
 console.log(personNew.membership());
+console.log(personNew.clientInfo());
+personNew.withdraw(100);
+console.log(personNew.clientInfo());
+console.log(personNew.getBalance());
+personNew.deposite(500);
+console.log(personNew.clientInfo());
+console.log(personNew.getBalance());
+
+//business
+const Business = function(name, balance, phone, category){
+    Client.call(this, name, balance);
+    this.phone = phone;
+    this.category = category;
+};
+
+//inherit the prototypes
+Business.prototype = Object.create(Client.prototype);
+//return constructor as business
+Business.prototype.constructor = Business;
+
+Business.prototype.clientInfo = function(){
+    return `Name: ${this.name}, balance: ${this.balance}, 
+    membership: ${this.membership()}, category: ${this.category}, 
+    phone: ${this.phone}`
+};
+//create a business
+const business = new Business('Udemy', 1000000, 244635980, 'Education');
+console.log(business);
+console.log(business.clientInfo());
+
+
 
 //string
 const name1 = 'Karen';
@@ -66,3 +132,36 @@ const array1= [1,2,3,4];
 const array2 = new Array(1,2,3,4);
 console.log(array1);
 console.log(array2);
+
+//object create
+const Client2 = {
+    getBalance : function(){
+        return `Hello ${this.name} your balance is ${this.balance}`
+    },
+    withdraw: function(amount){
+        return this.balance -= amount;
+    },
+    deposit: function(amount){
+        return this.balance += amount;
+    }
+};
+//create na new object and give a balance
+const mary = Object.create(Client2);
+//attach the properties
+mary.name = "Mary";
+mary.balance = 1000;
+console.log(mary);
+console.log(mary.getBalance());
+//withdraw some money
+mary.withdraw(500);
+console.log(mary.getBalance());
+//deposite some money
+mary.deposit(300);
+console.log(mary.getBalance());
+
+//another method to create object - not recomended
+const kasia = Object.create(Client2, {
+    name:{value: "Kasia"},
+    balance: {value: 1500}
+});
+console.log(kasia);
