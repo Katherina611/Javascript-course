a3672209b9de8d1378666e2c82bb61677a98626b
Katherina611 <katarzynag0609@gmail.com>
2018-03-28 15:48:41 +0200

    budgetLeft, warnings

--- weekBudget/js/app.js
+++ weekBudget/js/app.js
@@ -4,6 +4,11 @@ class Budget{
         this.budget = Number(budget);
         this.budgetLeft = this.budget;
     }
+    //substrack from the budget
+    substractFromBudget(amount){
+        return this.budgetLeft -= amount;
+    }
+
 }
 //everything related to HTML
 class HTML {
@@ -12,6 +17,48 @@ class HTML {
         //insert into HTML
         budgetTotal.innerHTML = `${amount}`;
         budgetLeft.innerHTML = `${amount}`;
+    }
+    //displays a message (corrnect or invalid
+    printMessage(message, className){
+        const messageWrapper = document.createElement('div');
+        messageWrapper.classList.add('text-center','alert', className);
+        messageWrapper.appendChild(document.createTextNode(message));
+        //insert into HTML
+        document.querySelector('.primary').insertBefore(messageWrapper,addExpenseForm);
+        //clear the error
+        setTimeout(function(){
+            document.querySelector('.primary .alert').remove();
+            addExpenseForm.reset();
+        },3000)
+    }
+    //displays the expenses from the form into the list
+    addExpenseToList(name, amount){
+        const expensesList = document.querySelector('#expenses ul');
+        //creat a <li>
+        const li = document.createElement('li');
+        li.className = 'list-group-item d-flex justify-content-between align-items-center';
+        //create the template
+        li.innerHTML = `
+        ${name}
+        <span class = "badge badge-primary badge-pill">$ ${amount}</span>
+        `;
+        //insert into the HTML
+        expensesList.appendChild(li);
+    }
+    //substract expense amount from budget
+    trackBudget(amount){
+        const budgetLeftDollars = budget.substractFromBudget(amount);
+        budgetLeft.innerHTML = `${budgetLeftDollars}`;
+        //check when 25% is left
+        if((budget.budget / 4) > budgetLeftDollars){
+            budgetLeft.parentElement.parentElement.classList.remove('alert-success', 'alert-warning');
+            budgetLeft.parentElement.parentElement.classList.add('alert-danger');
+        }else if((budget.budget / 2) > budgetLeftDollars){
+            budgetLeft.parentElement.parentElement.classList.remove('alert-success');
+            budgetLeft.parentElement.parentElement.classList.add('alert-warning');
+
+
+        }
     }
 }
 
@@ -43,7 +90,20 @@ function eventListeners(){
     });
     //when the new expence is added
     addExpenseForm.addEventListener("submit", function(event){
+        //read the input values
         event.preventDefault();
+        const expenseName = document.querySelector('#expense').value;
+        const amount = document.querySelector('#amount').value;
+
+        if(expenseName === '' || amount === ''){
+            html.printMessage('There was error, all the fields are mendatory', 'alert-danger');
+        }else{
+            //add expenses into the list
+            html.addExpenseToList(expenseName, amount);
+            html.trackBudget(amount);
+            html.printMessage('Added...', 'alert-success');
+        }
+
     });
 
 }
