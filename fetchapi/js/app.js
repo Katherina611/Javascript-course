db740882029dcfc80eff7a2205aee18f07b91442
Katherina611 <katarzynag0609@gmail.com>
2018-04-04 16:58:46 +0200

    fetch init

---
+++ fetchapi/js/app.js
@@ -0,0 +1,17 @@
+document.getElementById('button1').addEventListener('click', loadTxt);
+
+//load txt
+function loadTxt(){
+    fetch('data.txt')
+        .then(function (response) {
+            return response.text()
+        })
+        .then(function(data){
+            console.log(data);
+            document.getElementById('result').innerHTML = data;
+
+        })
+        .catch(function(error){
+            console.log(error);
+        })
+}
