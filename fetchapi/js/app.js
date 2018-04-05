b1fc5a48b75ce4333f9ba9e839d83b743b620647
Katherina611 <katarzynag0609@gmail.com>
2018-04-05 11:18:15 +0200

    json, jsonAPI

--- fetchapi/js/app.js
+++ fetchapi/js/app.js
@@ -1,4 +1,7 @@
 document.getElementById('button1').addEventListener('click', loadTxt);
+document.getElementById('button2').addEventListener('click', loadJSON);
+document.getElementById('button3').addEventListener('click', loadJSONAPI);
+
 
 //load txt
 function loadTxt(){
@@ -14,4 +17,50 @@ function loadTxt(){
         .catch(function(error){
             console.log(error);
         })
+}
+//load and print JSON
+function loadJSON(){
+    fetch('employees.json')
+        .then(function(response){
+            return response.json()
+        })
+        .then(function(data){
+            console.log(data);
+            let html = '';
+            data.forEach(function(employee){
+                html += `
+                <li>${employee.name} - ${employee.job}</li>
+                `;
+            });
+            //insert into the HTML
+            document.getElementById('result').innerHTML = html;
+        })
+        .catch(function(error){
+            console.log(error);
+        })
+}
+//prints the response from a REST API
+function loadJSONAPI(){
+    fetch('https://picsum.photos/list')
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(images){
+            let html = '';
+            images.forEach(function(image){
+                html += `
+                    <li>
+                        <a target = '_blank' href="${image.post_url}">View image</a>
+                        ${image.author}
+                    </li>
+                `;
+            });
+            //insert into the HTML
+            document.getElementById('result').innerHTML = html;
+        })
+        .catch(function(error){
+            console.log(error);
+        })
+
+
 }
