51533e5c886cf91997b37c7f9641819a1fe4def8
Katherina611 <katarzynag0609@gmail.com>
2018-04-09 14:44:09 +0200

    async await

--- asynchronusJS_arrowFunction/app.js
+++ asynchronusJS_arrowFunction/app.js
@@ -71,4 +71,37 @@ console.log(productQuantityLength);
 //arrow function with forEach
 shoppingCart.forEach(product => console.log(product));
 
+//async await
+async function getClients(){
+    //create a new promise
+    const clients = new Promise((resolve, reject) =>{
+        setTimeout(()=>{
+            resolve('Client list downloaded ...')
+
+        },1000)
+    });
+    const error = false;
+    if(!error){
+        const response = await clients;//hild until clients is executed
+        return response;
+    }else{
+        await Promise.reject('There was an error')
+    }
+}
+getClients()
+    .then(response => console.log(response))
+    .catch(error => console.log(error));
+
+//async await with a REST API
+async  function getPosts(){
+    //wait until the post are downloaded
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+    //execute then
+    const data = await response.json();
+    //console log until the second await finishececuting
+    return data;
+}
+getPosts()
+    .then(post => console.log(post));
+
 
