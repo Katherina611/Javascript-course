e87678409e318598a41b565c283f60a43890b787
Katherina611 <katarzynag0609@gmail.com>
2018-04-16 20:13:12 +0200

    desctrucrturing an object

--- Desctructuring_ModernJS/app.js
+++ Desctructuring_ModernJS/app.js
@@ -12,14 +12,49 @@ console.log(memberhip);
 */
 
 //the new way
-let name, membership;
+//let name, membership;
 
 const client = {
     name:'Kasia',
-    membership:'Premium'
+    membership:'Premium',
+    data:{
+        clientLocation:{
+            city:'Mexico',
+            country:'Mexico'
+        },
+        account:{
+            memberSince:'10-02-2012',
+            balance:4000
+        }
+    }
 };
+/*
 name = "Mary";
 membership = 'Platinum';
 ({name,membership} = client);
 console.log(name);
 console.log(membership);
+*/
+
+//extract object that's inside another object
+
+let {data:{clientLocation}} = client;
+console.log(clientLocation);
+console.log(clientLocation.city);
+console.log(clientLocation.country);
+
+let {data:{account}} = client;
+console.log(account);
+console.log(account.memberSince);
+console.log(account.balance);
+
+//default values when destructuring
+const client1 = {
+    name:'Kasia'
+};
+console.log(client1);
+//desctructuring
+let {name, membership = 'Standard', balance = 200} = client1;
+console.log(name);
+console.log(membership);
+console.log(balance);
