67604b61baa7c65776e4f3675706c4ae3f1f7323
Katherina611 <katarzynag0609@gmail.com>
2018-04-24 13:38:28 +0200

    information from rest api

--- cocktail/js/UI.js
+++ cocktail/js/UI.js
@@ -1,6 +1,6 @@
 class UI{
     //displays drinks with ingredients
-    displayDrinksWithIngerdients(drink){
+    displayDrinksWithIngerdients(drinks){
         //show the results
         const resultsWrapper = document.querySelector('.results-wrapper');
         resultsWrapper.style.display = 'block';
@@ -10,20 +10,21 @@ class UI{
             resultsDiv.innerHTML += `
             <div class="col-md-6">
                 <div class="card my-3">
-                    <img class="card-img-top" src="" alt="">
+                    <img class="card-img-top" src="${drink.strDrinkThumb}" alt="${drink.strDrink}">
                     <div class="card-body">
-                        <h2 class="card-title text-center"></h2>
+                        <h2 class="card-title text-center">${drink.strDrink}</h2>
                         <p class="card-text font-weight-bold">Instructions: </p>
-                        <p class="card-text"></p>
+                        <p class="card-text">${drink.strInstructions}</p>
                         <p class="card-text">
                             <ul class="list-group">
                                 <li class="list-group-item alert alert-danger">Ingredients: </li>
+                                ${this.displayIngredients(drink)}
                             </ul>
                         </p>
                         <p class="card-text font-weight-bold">Extra information: </p>
                         <p class="card-text">
-                            <span class="badge badge-pill badge-success"></span>
-                            <span class="badge badge-pill badge-warning">Category: </span>
+                            <span class="badge badge-pill badge-success">${drink.strAlcoholic}</span>
+                            <span class="badge badge-pill badge-warning">Category: ${drink.strCategory} </span>
                         </p>
                     </div>
                 </div>
@@ -31,9 +32,31 @@ class UI{
                 
             `
         })
-
-
     }
+
+    //prints the ingredients and measurements
+    displayIngredients(drink){
+
+        let ingredients =[];
+        for (let i = 1; i < 16; i++ ){
+            const ingredientMeasure = {};
+            if(drink[`strIngredient${i}`] !== ''){
+                ingredientMeasure.ingredient = drink[`strIngredient${i}`];
+                ingredientMeasure.measure = drink[`strMeasure${i}`];
+                ingredients.push(ingredientMeasure);
+            }
+        }
+        //console.log(ingredients);
+        //build the template
+        let ingredientsTemplate  = '';
+        ingredients.forEach(ingredient =>{
+            ingredientsTemplate += `
+            <li class="list-group-item">${ingredient.ingredient} - ${ingredient.measure}</li>
+            `
+        });
+        return ingredientsTemplate;
+    }
+
     //displays a custom message
     printMessage(message, className){
         const div = document.createElement('div');
