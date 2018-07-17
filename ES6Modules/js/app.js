d9091c689ca6c309b14d4cc6d32ac3100736d4d1
Katherina611 <katarzynag0609@gmail.com>
2018-07-17 14:13:34 +0200

    export class

--- ES6Modules/js/app.js
+++ ES6Modules/js/app.js
@@ -1,5 +1,5 @@
 //import from client.js
-import{clientName, amount, displayClientInfo} from "./client.js";
+import{clientName, amount, Client} from "./client.js";
 console.log(clientName);
 console.log(amount);
 /*
@@ -9,5 +9,9 @@ console.log(clientInfo.amount);
 */
 
 //use the function
-let client = displayClientInfo(clientName, amount);
-console.log(client);
+//let client = displayClientInfo(clientName, amount);
+//console.log(client);
+
+//use the class
+let client = new Client("Kasia", 20);
+console.log(client.displayClientInfo());
--- ES6Modules/js/client.js
+++ ES6Modules/js/client.js
@@ -6,7 +6,18 @@ export let amount = 200;
 export function displayClientInfo(name, amount){
     return `Clinet: ${name} - Amount: ${amount}`;
 }
-*/
+
 export const displayClientInfo = (name, amount) =>{
-    return `Clinet: ${name} - Amount: ${amount}`;
+    return `Client: ${name} - Amount: ${amount}`;
+}
+*/
+//create the client class
+export class Client{
+    constructor(name, amount){
+        this.name = name;
+        this.amount = amount;
+    }
+    displayClientInfo(){
+        return `Clinet: ${this.name} - Amount: ${this.amount}`;
+    }
 }
