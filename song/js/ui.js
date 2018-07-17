e0d51ea0ff3484148083ebbd94cff9fc7a3be0f1
Katherina611 <katarzynag0609@gmail.com>
2018-07-17 14:33:10 +0200

    import UI

---
+++ song/js/ui.js
@@ -0,0 +1,4 @@
+export const searchForm = document.querySelector('#search-form'),
+             searchDiv = document.querySelector('#search'),
+             resultDIv = document.querySelector('#result'),
+             messageDiv = document.querySelector('#messages');
