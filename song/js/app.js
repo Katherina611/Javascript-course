9515cf5334c0b94d60b7d40adc558f4b8ae05cf3
Katherina611 <katarzynag0609@gmail.com>
2018-07-18 12:08:57 +0200

    rest api

--- song/js/app.js
+++ song/js/app.js
@@ -1,3 +1,4 @@
+import {API} from './api.js';
 import * as UI from "./ui.js";
 
 UI.searchForm.addEventListener('submit', (e)=>{
@@ -15,5 +16,12 @@ UI.searchForm.addEventListener('submit', (e)=>{
         },3000);
     }else{
         //query the REST API
+        const lyric = new API(artistName, songName);
+        lyric.queryAPI()
+            .then(data =>{
+                let result = data.lyric.lyrics;
+                UI.resultDIv.textContent = result;
+            })
+        
     }
 })
