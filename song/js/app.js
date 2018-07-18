ba67492d1eb9e5519b4356efcd2955ee93c9b78b
Katherina611 <katarzynag0609@gmail.com>
2018-07-18 11:50:43 +0200

    validate the form

--- song/js/app.js
+++ song/js/app.js
@@ -1,3 +1,19 @@
 import * as UI from "./ui.js";
 
-console.log(UI);
+UI.searchForm.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    //read the form data
+    const artistName = UI.artistInput.value,
+          songName = UI.songInput.value;
+    //validate the form 
+    if(artistName === "" || songName ===""){
+        UI.messageDiv.innerHTML = "Error... All fields are mendatory";
+        UI.messageDiv.classList.add('error');
+        setTimeout(()=>{
+            UI.messageDiv.innerHTML = "";
+            UI.messageDiv.classList.remove('error')
+        },3000);
+    }else{
+        //query the REST API
+    }
+})
